ba5a688b887d58f34de580fd9cf55e8690a0007a
Afsharov <afschar@informatik.uni-frankfurt.de>
2019-10-17 20:08:29 +0200

    Add code for performance metrics

--- model_tests.R
+++ model_tests.R
@@ -5,7 +5,7 @@ library(e1071)
 
 
 data <- read.csv(file="datasets/training_data.csv")
-# test.data <- read.csv(file="datasets/testing_data.csv")
+test.data <- read.csv(file="datasets/testing_data.csv")
 
 set.seed(3)
 train = sample(nrow(data), nrow(data)*2/3)
@@ -25,18 +25,42 @@ prediction_dt <- predict(tree.data_train, data_test, type="class")
 res_dt <- table(prediction_dt, data_test$class)
 res_dt
 
+dt.accuracy <- (res_dt[1,1] + res_dt[2,2])/sum(res_dt)
+dt.precision <- res_dt[1,1]/(res_dt[1,1]+res_dt[2,1])
+dt.recall <- res_dt[1,1]/(res_dt[1,1]+res_dt[1,2])
+
+dt.accuracy
+dt.precision
+dt.recall
+
 #random forests
 set.seed(3)
-rf.data <- randomForest(as.factor(data_train$class)~., data_train, ntree=3)
+rf.data <- randomForest(as.factor(data_train$class)~., data_train, ntree=10)
 
 prediction_rf <- predict(rf.data, data_test, type="class")
 res_rf <- table(prediction_rf, data_test$class)
 res_rf
 
+rf.accuracy <- (res_rf[1,1] + res_rf[2,2])/sum(res_rf)
+rf.precision <- res_rf[1,1]/(res_rf[1,1]+res_rf[2,1])
+rf.recall <- res_rf[1,1]/(res_rf[1,1]+res_rf[1,2])
+
+rf.accuracy
+rf.precision
+rf.recall
+
 #support vector machines
 set.seed(3)
-svm.data <- svm(as.factor(data_train$class)~., data_train, kernel = "linear")
+svm.data <- svm(as.factor(data_train$class)~., data_train, kernel = "polynomial")
 
 prediction_svm <- predict(svm.data, data_test, type="class")
 res_svm <- table(prediction_svm, data_test$class)
 res_svm
+
+accuracy <- (res_svm[1,1] + res_svm[2,2])/sum(res_svm)
+precision <- res_svm[1,1]/(res_svm[1,1]+res_svm[2,1])
+recall <- res_svm[1,1]/(res_svm[1,1]+res_svm[1,2])
+
+accuracy
+precision
+recall
