bb610f1772f6aa2d058c6ba5c8d1cfb9b21e6db2
Afsharov <afschar@informatik.uni-frankfurt.de>
2019-10-21 14:24:41 +0200

    Calculate all metrics in for-loop

--- model_tests.R
+++ model_tests.R
@@ -6,6 +6,10 @@ library(e1071)
 training_data <- read.csv(file="datasets/training_data.csv")
 test_data <- read.csv(file="datasets/testing_data.csv")
 
+calculate_performance_table <- fun(conmatrix, classnum) {
+  
+}
+
 ########## decision trees ##########
 set.seed(3)
 tree.data_train = tree(as.factor(training_data$class)~., training_data)
@@ -15,18 +19,29 @@ res_dt <- table(prediction_dt, test_data$class)
 res_dt
 
 # performance table
-tree.performance <- matrix( rep( 0, len=15), nrow = 5)
-colnames(tree.performance) <- c("Accuracy", "Precision", "Recall")
+tree.performance <- matrix( rep( 0, len=30), nrow = 5)
+colnames(tree.performance) <- c("Precision", "Recall", "F1-Score", "TP", "FP", "FN")
 rownames(tree.performance) <- c("Class0", "Class1", "Class2", "Class3", "Class4")
 
+# claculate all metrics and fill the table
 tree.accuracy <- (res_dt[1,1] + res_dt[2,2] + res_dt[3,3] + res_dt[4,4] + res_dt[5,5])/sum(res_dt)
-for (i in 1:5) {
-  tree.performance[i,1] <- tree.accuracy
-}
- 
-dt.precision <- res_dt[1,1]/(res_dt[1,1]+res_dt[2,1])
-dt.recall <- res_dt[1,1]/(res_dt[1,1]+res_dt[1,2])
 
+for (i in 1:5) {
+  # precison
+  tree.performance[i,1] <- res_dt[i,i]/sum(res_dt[,i])
+  # recall
+  tree.performance[i,2] <- res_dt[i,i]/sum(res_dt[i,])
+  # f1-score
+  tree.performance[i,3] <- 2 * ((tree.performance[i,1] * tree.performance[i,2])/(tree.performance[i,1] + tree.performance[i,2]))
+  # true positives
+  tree.performance[i,4] <- res_dt[i,i]
+  # false positives
+  tree.performance[i,5] <- sum(res_dt[,i]) - res_dt[i,i]
+  # false negatives
+  tree.performance[i,6] <- sum(res_dt[i,]) - res_dt[i,i]
+}
+
+tree.performance <- as.table(tree.performance)
 tree.performance
 
 ########## random forests ##########
