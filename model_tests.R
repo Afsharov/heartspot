dbc75d3b774999b50b167195631664386b1bdfea
Afsharov <afschar@informatik.uni-frankfurt.de>
2019-10-21 09:29:16 +0200

    Summarize performance in one table

--- model_tests.R
+++ model_tests.R
@@ -3,31 +3,33 @@ library(ISLR)
 library(randomForest)
 library(e1071)
 
-
 training_data <- read.csv(file="datasets/training_data.csv")
 test_data <- read.csv(file="datasets/testing_data.csv")
 
-#decision trees
+########## decision trees ##########
 set.seed(3)
 tree.data_train = tree(as.factor(training_data$class)~., training_data)
-
-summary(tree.data_train)
-plot(tree.data_train)
-text(tree.data_train ,pretty =0)
 
 prediction_dt <- predict(tree.data_train, test_data, type="class")
 res_dt <- table(prediction_dt, test_data$class)
 res_dt
 
-dt.accuracy <- (res_dt[1,1] + res_dt[2,2] + res_dt[3,3] + res_dt[4,4] + res_dt[5,5])/sum(res_dt)
+# performance table
+tree.performance <- matrix( rep( 0, len=15), nrow = 5)
+colnames(tree.performance) <- c("Accuracy", "Precision", "Recall")
+rownames(tree.performance) <- c("Class0", "Class1", "Class2", "Class3", "Class4")
+
+tree.accuracy <- (res_dt[1,1] + res_dt[2,2] + res_dt[3,3] + res_dt[4,4] + res_dt[5,5])/sum(res_dt)
+for (i in 1:5) {
+  tree.performance[i,1] <- tree.accuracy
+}
+ 
 dt.precision <- res_dt[1,1]/(res_dt[1,1]+res_dt[2,1])
 dt.recall <- res_dt[1,1]/(res_dt[1,1]+res_dt[1,2])
 
-dt.accuracy
-dt.precision
-dt.recall
+tree.performance
 
-#random forests
+########## random forests ##########
 set.seed(3)
 rf.data <- randomForest(as.factor(training_data$class)~., training_data, ntree=10)
 
@@ -43,7 +45,7 @@ rf.accuracy
 rf.precision
 rf.recall
 
-#support vector machines
+########## support vector machines ##########
 set.seed(3)
 svm.data <- svm(as.factor(training_data$class)~., training_data, kernel = "polynomial")
 
