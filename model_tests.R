6c90dcd79ba1a812740ca85a1bfdaed454a50492
Afsharov <afschar@informatik.uni-frankfurt.de>
2019-10-21 14:50:48 +0200

    Define function to calculate all metrics

--- model_tests.R
+++ model_tests.R
@@ -6,8 +6,35 @@ library(e1071)
 training_data <- read.csv(file="datasets/training_data.csv")
 test_data <- read.csv(file="datasets/testing_data.csv")
 
-calculate_performance_table <- fun(conmatrix, classnum) {
+calculate_performance_table <- function(conmatrix, classnum) {
+  # transpose matrix
+  conmatrix <- t(conmatrix)
   
+  # performance table
+  performance <- matrix( rep( 0, len=(6*classnum)), nrow = classnum)
+  colnames(performance) <- c("Precision", "Recall", "F1-Score", "TP", "FP", "FN")
+  rownames(performance) <- c("Class0", "Class1", "Class2", "Class3", "Class4")
+  
+  # claculate all metrics and fill the table
+  accuracy <- (conmatrix[1,1] + conmatrix[2,2] + conmatrix[3,3] + conmatrix[4,4] + conmatrix[5,5])/sum(conmatrix)
+  
+  for (i in 1:classnum) {
+    # precison
+    performance[i,1] <- conmatrix[i,i]/sum(conmatrix[,i])
+    # recall
+    performance[i,2] <- conmatrix[i,i]/sum(conmatrix[i,])
+    # f1-score
+    performance[i,3] <- 2 * ((performance[i,1] * performance[i,2])/(performance[i,1] + performance[i,2]))
+    # true positives
+    performance[i,4] <- conmatrix[i,i]
+    # false positives
+    performance[i,5] <- sum(conmatrix[,i]) - conmatrix[i,i]
+    # false negatives
+    performance[i,6] <- sum(conmatrix[i,]) - conmatrix[i,i]
+  }
+  
+  performance <- as.table(performance)
+  return(performance)
 }
 
 ########## decision trees ##########
@@ -18,30 +45,7 @@ prediction_dt <- predict(tree.data_train, test_data, type="class")
 res_dt <- table(prediction_dt, test_data$class)
 res_dt
 
-# performance table
-tree.performance <- matrix( rep( 0, len=30), nrow = 5)
-colnames(tree.performance) <- c("Precision", "Recall", "F1-Score", "TP", "FP", "FN")
-rownames(tree.performance) <- c("Class0", "Class1", "Class2", "Class3", "Class4")
-
-# claculate all metrics and fill the table
-tree.accuracy <- (res_dt[1,1] + res_dt[2,2] + res_dt[3,3] + res_dt[4,4] + res_dt[5,5])/sum(res_dt)
-
-for (i in 1:5) {
-  # precison
-  tree.performance[i,1] <- res_dt[i,i]/sum(res_dt[,i])
-  # recall
-  tree.performance[i,2] <- res_dt[i,i]/sum(res_dt[i,])
-  # f1-score
-  tree.performance[i,3] <- 2 * ((tree.performance[i,1] * tree.performance[i,2])/(tree.performance[i,1] + tree.performance[i,2]))
-  # true positives
-  tree.performance[i,4] <- res_dt[i,i]
-  # false positives
-  tree.performance[i,5] <- sum(res_dt[,i]) - res_dt[i,i]
-  # false negatives
-  tree.performance[i,6] <- sum(res_dt[i,]) - res_dt[i,i]
-}
-
-tree.performance <- as.table(tree.performance)
+tree.performance <- calculate_performance_table(res_dt, 5)
 tree.performance
 
 ########## random forests ##########
@@ -52,13 +56,8 @@ prediction_rf <- predict(rf.data, test_data, type="class")
 res_rf <- table(prediction_rf, test_data$class)
 res_rf
 
-rf.accuracy <- (res_rf[1,1] + res_rf[2,2] + res_rf[3,3] + res_rf[4,4] + res_rf[5,5])/sum(res_rf)
-rf.precision <- res_rf[1,1]/(res_rf[1,1]+res_rf[2,1])
-rf.recall <- res_rf[1,1]/(res_rf[1,1]+res_rf[1,2])
-
-rf.accuracy
-rf.precision
-rf.recall
+rf.performance <- calculate_performance_table(res_rf, 5)
+rf.performance
 
 ########## support vector machines ##########
 set.seed(3)
@@ -68,10 +67,5 @@ prediction_svm <- predict(svm.data, test_data, type="class")
 res_svm <- table(prediction_svm, test_data$class)
 res_svm
 
-accuracy <- (res_svm[1,1] + res_svm[2,2] + res_svm[3,3] + res_svm[4,4] + res_svm[5,5])/sum(res_svm)
-precision <- res_svm[1,1]/(res_svm[1,1]+res_svm[2,1])
-recall <- res_svm[1,1]/(res_svm[1,1]+res_svm[1,2])
-
-accuracy
-precision
-recall
+svm.performance <- calculate_performance_table(res_svm, 5)
+svm.performance
