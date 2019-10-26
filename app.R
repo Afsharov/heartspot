f805a2735db7b803eecb32c1b1c59a64bda1763d
Afsharov <afschar@informatik.uni-frankfurt.de>
2019-10-26 14:42:32 +0200

    Add tabs to front-end

--- app.R
+++ app.R
@@ -9,43 +9,60 @@ normalize <- function(x) {
 # Define UI (fluidPage is a flexible default choice) ----
 ui <- dashboardPage(skin = "red",
   dashboardHeader(title = "HeartSpot"),
-  dashboardSidebar(disable = TRUE
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Information", tabName = "info", icon = icon("info")),
+      menuItem("Prediction", tabName = "prediction", icon = icon("heartbeat"))
+    )
   ),
   dashboardBody(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
     ),
-    fluidRow(
-      column(width = 8,
-        h2("Prediciton"),
-        box(width = NULL, status = "danger",
-          plotOutput('plot1'),
-          submitButton(text="submit", icon=NULL)
-        ),
-        h2("Interpretation"),
-        box(width = NULL, status = "danger",
-          textOutput("text")
+    tabItems(
+      tabItem(tabName = "info",
+        column(width = 6,
+          box(width = NULL, status = "warning",
+             HTML("<h2 style='text-align: center; color:red; font-weight: bold;'>HeartSpot</h2>"),
+             HTML("<img src='image4.png' style='display: block; margin-left: auto; margin-right: auto; width: 80%;'/>"),
+             HTML("<p style='text-align: center; color:gray; font-weight: bold; font-style: italic;'>AI-powered Heart Disease prediction tool</p>")
+          )
         )
       ),
-      column(width = 4,
-        h2("Data Input"),
-        box(width = NULL, title = "Demographics", status = "danger",
-          textInput('Age', 'Age','25', width = "50%"),  
-          selectInput('Sex', 'Gender',  c('male','female', 'unknown'), width = "50%")
-        ),
-        box(width = NULL, title = "Symptoms", status = "danger",
-          selectInput('CP', 'Chest Pain',  c('typical angina','atypical angina', 'non-anginal pain', 'asymptomatic'), width = "75%"),
-          selectInput('exang', 'Exercise-induced angina', c('no','yes'), width = "75%")
-        ),
-        box(width = NULL, title = "ECG", status = "danger",
-          sliderInput('oldpeak', 'ST depression',0,5,3.0,0.2),
-          textInput('threstbps', 'Resting blood pressure in mmHg','80', width = "75%"),
-          textInput('thalach', 'Maximum heart rate achieved per minute','180', width = "75%"),
-          selectInput('restecg', 'Resting electrocardiographic results',  c('normal','having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV', 'showing probable or definite left ventricular hypertrophy by Estes\'\ criteria'), width = "75%")
-        ),
-        box(width = NULL, title = "Lab results", status = "danger",
-            sliderInput('chol', 'Serum cholesterol in mg/dl',0,20,3.5,0.2),
-            selectInput('fbs', 'Fasting blood glucose >120mg/dl', c('no','yes'), width = "50%")
+      tabItem(tabName = "prediction",
+        fluidRow(
+          column(width = 8,
+            h2("Prediciton"),
+            box(width = NULL, status = "danger",
+               plotOutput('plot1'),
+               submitButton(text="submit", icon=NULL)
+            ),
+            h2("Interpretation"),
+            box(width = NULL, status = "danger",
+               textOutput("text")
+            )
+          ),
+          column(width = 4,
+            h2("Data Input"),
+            box(width = NULL, title = "Demographics", status = "danger",
+               textInput('Age', 'Age','25', width = "50%"),  
+               selectInput('Sex', 'Gender',  c('male','female', 'unknown'), width = "50%")
+            ),
+            box(width = NULL, title = "Symptoms", status = "danger",
+               selectInput('CP', 'Chest Pain',  c('typical angina','atypical angina', 'non-anginal pain', 'asymptomatic'), width = "75%"),
+               selectInput('exang', 'Exercise-induced angina', c('no','yes'), width = "75%")
+            ),
+            box(width = NULL, title = "ECG", status = "danger",
+               sliderInput('oldpeak', 'ST depression',0,5,3.0,0.2),
+               textInput('threstbps', 'Resting blood pressure in mmHg','80', width = "75%"),
+               textInput('thalach', 'Maximum heart rate achieved per minute','180', width = "75%"),
+               selectInput('restecg', 'Resting electrocardiographic results',  c('normal','having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV', 'showing probable or definite left ventricular hypertrophy by Estes\'\ criteria'), width = "75%")
+            ),
+            box(width = NULL, title = "Lab results", status = "danger",
+               sliderInput('chol', 'Serum cholesterol in mg/dl',0,20,3.5,0.2),
+               selectInput('fbs', 'Fasting blood glucose >120mg/dl', c('no','yes'), width = "50%")
+            )
+          )
         )
       )
     )
